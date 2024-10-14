425f754f1e68417a495bb5f3c05f9eced98f58bb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-10-14 23:38:02 +0600

    Code improvements (#7)
    * add
    
    * 1
    
    * 2
    
    * Up
    
    * Up
    
    * Update stencil.config.ts
    
    * yep
    
    * Add
    
    * Update copy.py

--- src/components/django-hstore-widget/django-hstore-widget.tsx
+++ src/components/django-hstore-widget/django-hstore-widget.tsx
@@ -1,4 +1,6 @@
-import { Component, Host, h, Prop, State, Fragment } from '@stencil/core';
+import { Component, Host, h, Prop, State, Fragment, Watch } from '@stencil/core';
+
+const GITHUB_ISSUE_URL = 'https://github.com/baseplate-admin/django-hstore-widget/issues';
 
 @Component({
     tag: 'django-hstore-widget',
@@ -6,6 +8,7 @@ import { Component, Host, h, Prop, State, Fragment } from '@stencil/core';
     shadow: false,
 })
 export class DjangoHstoreWidget {
+    // Primary Attributes
     @Prop() json: string;
     @Prop() field_name: string;
 
@@ -18,171 +21,173 @@ export class DjangoHstoreWidget {
     @State() output_render_type: 'rows' | 'textarea' = 'rows';
 
     // Very Fragile state. Please update with care
-    @State() _json: Array<{ key: string; value: string; index: number }> = new Array();
-    @State() _json_string: string = '';
+    @State() _json = new Array<{ key: string; value: string; index: number }>();
 
-    connectedCallback() {
-        let json_object = JSON.parse(this.json);
-        let json = Object.keys(json_object).map((key, index) => ({
-            key: key,
-            value: json_object[key],
-            index: index,
-        }));
-        this._json = json;
-        this.updateJSONString();
+    // Watchers
+    @Watch('json')
+    jsonWatcher(newValue: string) {
+        this.#parseJson(newValue);
     }
 
-    private handleDelete(index: number) {
+    // Callbacks
+    connectedCallback() {
+        this.#parseJson(this.json);
+    }
+
+    // Functions
+    #parseJson(json_string: string) {
+        try {
+            let json_object = JSON.parse(json_string);
+            let json = Object.keys(json_object).map((key, index) => ({
+                key: key,
+                value: json_object[key],
+                index: index,
+            }));
+            this._json = json;
+        } catch (err) {
+            console.error(err);
+            this._json = [];
+        }
+    }
+
+    #handleDelete(index: number) {
         const json = this._json.filter(obj => obj.index !== index);
         this._json = structuredClone(json);
     }
 
-    private handleRowAdd() {
-        let json = this._json;
-        const last_item = json.at(-1);
-        if (!last_item) {
-            console.error('`this._json` is empty.');
-            return;
-        }
+    #handleRowAdd() {
+        const last_item = this._json.at(-1);
 
         const data = {
-            index: last_item.index + 1,
+            index: last_item ? last_item.index + 1 : 0,
             key: '',
             value: '',
         };
-        json.push(data);
-        this._json = structuredClone(json);
+        this._json.push(data);
+
+        this._json = structuredClone(this._json);
     }
 
-    private handleToggleClick() {
-        switch (this.output_render_type) {
-            case 'rows':
-                this.output_render_type = 'textarea';
-                break;
-            case 'textarea':
-                this.output_render_type = 'rows';
-                break;
-            default:
-                console.error('Something is wrong with `output_render_type`');
-                break;
+    #handleToggleClick() {
+        if (this.output_render_type === 'rows') {
+            this.output_render_type = 'textarea';
+        } else if (this.output_render_type === 'textarea') {
+            this.output_render_type = 'rows';
+        } else {
+            console.error('Something is wrong with `output_render_type`');
         }
     }
 
-    private getJSONWithoutIndex() {
-        return this._json.reduce((acc, curr) => {
+    #handleTextAreaInput(event: Event) {
+        const target = event.currentTarget as HTMLTextAreaElement;
+        const value = target.value;
+        this.json = value;
+        this.#parseJson(value);
+    }
+
+    #handleKeyInput(event: Event, item: (typeof this._json)[0]) {
+        const target = event.currentTarget as HTMLInputElement;
+        const value = target.value;
+        item.key = value;
+        this._json = structuredClone(this._json);
+    }
+
+    #handleValueInput(event: Event, item: (typeof this._json)[0]) {
+        const target = event.currentTarget as HTMLInputElement;
+        const value = target.value;
+        item.value = value;
+        this._json = structuredClone(this._json);
+    }
+
+    // Getters
+    #getJSONString() {
+        const jsonObject = this._json.reduce((acc, curr) => {
             acc[curr.key] = curr.value;
             return acc;
         }, {} as Record<string, string>);
-    }
-
-    private updateJSONString() {
-        this._json_string = JSON.stringify(this.getJSONWithoutIndex(), null, Object.keys(this._json).length === 1 ? 0 : 4);
-    }
-
-    private handleTextAreaInput(text: string) {
-        this.json = text;
-        this.connectedCallback();
+        return JSON.stringify(jsonObject, null, Object.keys(jsonObject).length === 1 ? 0 : 4);
     }
 
     render() {
-        return (
-            <Host>
-                <textarea
-                    class={`${this.output_render_type === 'textarea' ? '' : 'hidden'} vLargeTextField`}
-                    cols={40}
-                    name={`${this.field_name}`}
-                    rows={10}
-                    onInput={event => {
-                        const target = event.currentTarget as HTMLTextAreaElement;
-                        const value = target.value;
-                        this.handleTextAreaInput(value);
-                    }}
-                >
-                    {this._json_string}
-                </textarea>
+        if (this._json.length > 0 && Array.isArray(this._json)) {
+            return (
+                <Host>
+                    <textarea
+                        class={`${this.output_render_type === 'textarea' ? '' : 'hidden'} vLargeTextField`}
+                        cols={40}
+                        name={this.field_name}
+                        rows={10}
+                        onInput={event => this.#handleTextAreaInput(event)}
+                    >
+                        {this.#getJSONString()}
+                    </textarea>
 
-                {this.output_render_type === 'rows' && this._json && (
-                    <Fragment>
-                        {this._json.map((item, index) => {
-                            return (
-                                <div class="form-row field-data">
-                                    <div class="wrapper">
-                                        <input
-                                            value={item.key}
-                                            onInput={event => {
-                                                const target = event.currentTarget as HTMLInputElement;
-                                                const value = target.value;
-                                                item.key = value;
-                                                this.updateJSONString();
-                                            }}
-                                            placeholder="key"
-                                            class="left"
-                                        />
-                                        <strong>:</strong>
-                                        <input
-                                            value={item.value}
-                                            onInput={() => {
-                                                const target = event.currentTarget as HTMLInputElement;
-                                                const value = target.value;
-                                                item.value = value;
-                                                this.updateJSONString();
-                                            }}
-                                            placeholder="value"
-                                            class="right"
-                                        />
-                                        <div
-                                            class="centered"
-                                            onClick={() => {
-                                                this.handleDelete(index);
-                                            }}
-                                        >
-                                            <img src={this.delete_svg_src} alt="❌" />
+                    {this.output_render_type === 'rows' && this._json && (
+                        <Fragment>
+                            {this._json.map(item => {
+                                return (
+                                    <div class="form-row field-data" key={item.index}>
+                                        <div class="wrapper">
+                                            <input value={item.key} onInput={event => this.#handleKeyInput(event, item)} placeholder="key" class="left" />
+                                            <strong>:</strong>
+                                            <input value={item.value} onInput={event => this.#handleValueInput(event, item)} placeholder="value" class="right" />
+                                            <div class="items-center justify-center flex" onClick={() => this.#handleDelete(item.index)}>
+                                                <img src={this.delete_svg_src} alt="❌" />
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            );
-                        })}
-                    </Fragment>
-                )}
-                <div class="form-row end-items">
-                    {this.output_render_type === 'rows' ? (
-                        <Fragment>
-                            <div
-                                class="centered gap-1"
-                                onClick={() => {
-                                    this.handleRowAdd();
-                                }}
-                            >
-                                <img src={this.add_svg_src} alt="➕" />
-                                Add row
-                            </div>
-                        </Fragment>
-                    ) : (
-                        <Fragment>
-                            <div></div>
+                                );
+                            })}
                         </Fragment>
                     )}
-                    <div
-                        class="centered gap-1"
-                        onClick={() => {
-                            this.handleToggleClick();
-                        }}
-                    >
-                        {this.output_render_type === 'textarea' && (
+                    <div class="form-row end-items flex">
+                        {this.output_render_type === 'rows' ? (
                             <Fragment>
-                                <img src={this.delete_svg_src} alt="❌" />
-                                Close TextArea
+                                <div class="items-center justify-center flex gap-1" onClick={() => this.#handleRowAdd()}>
+                                    <img src={this.add_svg_src} alt="➕" />
+                                    Add row
+                                </div>
+                            </Fragment>
+                        ) : (
+                            <Fragment>
+                                <div></div>
                             </Fragment>
                         )}
-                        {this.output_render_type === 'rows' && (
-                            <Fragment>
-                                <img src={this.edit_svg_src} alt="✏️" />
-                                Open TextArea
-                            </Fragment>
-                        )}
+                        <div
+                            class="items-center justify-center flex gap-1"
+                            onClick={() => {
+                                this.#handleToggleClick();
+                            }}
+                        >
+                            {this.output_render_type === 'textarea' && (
+                                <Fragment>
+                                    <img src={this.delete_svg_src} alt="❌" />
+                                    Close TextArea
+                                </Fragment>
+                            )}
+                            {this.output_render_type === 'rows' && (
+                                <Fragment>
+                                    <img src={this.edit_svg_src} alt="✏️" />
+                                    Open TextArea
+                                </Fragment>
+                            )}
+                        </div>
                     </div>
-                </div>
-            </Host>
-        );
+                </Host>
+            );
+        } else {
+            return (
+                <Host>
+                    <div class="flex items-center justify-center gap-1">
+                        <p>
+                            The provided json is <code>{this.json}</code> which is not valid.
+                        </p>
+                        <p>
+                            Please check the the json or <a href={GITHUB_ISSUE_URL}>file an issue at Github</a>
+                        </p>
+                    </div>
+                </Host>
+            );
+        }
     }
 }
