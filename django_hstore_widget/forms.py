4560afd0c757cdafd1a7084f569f81fa011197fb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-10-31 18:33:41 +0600

    add (#29)
    * add
    
    * Add

--- django_hstore_widget/forms.py
+++ django_hstore_widget/forms.py
@@ -4,6 +4,9 @@ from django.contrib.postgres.forms import HStoreField
 from .widgets import HStoreFormWidget
 
 
+from django.utils.html import format_html, html_safe
+
+
 class HStoreFormField(HStoreField):
     widget = HStoreFormWidget
 
