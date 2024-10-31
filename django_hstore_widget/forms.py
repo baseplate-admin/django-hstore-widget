399cb84e82e1139f5884542acf47175d6d515901
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-10-31 18:22:51 +0600

    Add

--- django_hstore_widget/forms.py
+++ django_hstore_widget/forms.py
@@ -6,47 +6,6 @@ from .widgets import HStoreFormWidget
 
 from django.utils.html import format_html, html_safe
 
-try:
-    from django.contrib.admin.templatetags.admin_static import static
-except ImportError:
-    from django.templatetags.static import static
-
-
-@html_safe
-class Asset:
-    """A generic asset that can be included in a template."""
-
-    def __init__(self, path):
-        self.path = path
-
-    def __eq__(self, other):
-        return (self.__class__ is other.__class__ and self.path == other.path) or (
-            other.__class__ is str and self.path == other
-        )
-
-    def __hash__(self):
-        return hash(self.path)
-
-    def __str__(self):
-        return self.absolute_path(self.path)
-
-    def absolute_path(self, path):
-        if path.startswith(("http://", "https://", "/")):
-            return path
-        return static(path)
-
-    def __repr__(self):
-        return f"{type(self).__qualname__}: {self.path!r}"
-
-
-class ESM(Asset):
-    """A JavaScript asset for ECMA Script Modules (ESM)."""
-
-    def __str__(self):
-        path = super().__str__()
-        template = '<script src="{}" type="module"></script>'
-        return format_html(template, self.absolute_path(path))
-
 
 class HStoreFormField(HStoreField):
     widget = HStoreFormWidget
@@ -55,6 +14,3 @@ class HStoreFormField(HStoreField):
         if not value:
             value = "{}"
         return json.loads(value)
-
-    class Media:
-        js = [ESM("admin/js/django_hstore_widget/django-hstore-widget.esm.js")]
--- django_hstore_widget/widgets.py
+++ django_hstore_widget/widgets.py
@@ -2,12 +2,54 @@ from django.contrib.admin.widgets import AdminTextareaWidget
 from django.template.loader import get_template
 from django.utils.safestring import mark_safe
 
+from django.utils.html import format_html, html_safe
+
+try:
+    from django.contrib.admin.templatetags.admin_static import static
+except ImportError:
+    from django.templatetags.static import static
+
+
+@html_safe
+class Asset:
+    """A generic asset that can be included in a template."""
+
+    def __init__(self, path):
+        self.path = path
+
+    def __eq__(self, other):
+        return (self.__class__ is other.__class__ and self.path == other.path) or (
+            other.__class__ is str and self.path == other
+        )
+
+    def __hash__(self):
+        return hash(self.path)
+
+    def __str__(self):
+        return self.absolute_path(self.path)
+
+    def absolute_path(self, path):
+        if path.startswith(("http://", "https://", "/")):
+            return path
+        return static(path)
+
+    def __repr__(self):
+        return f"{type(self).__qualname__}: {self.path!r}"
+
+
+class ESM(Asset):
+    """A JavaScript asset for ECMA Script Modules (ESM)."""
+
+    def __str__(self):
+        path = super().__str__()
+        template = '<script src="{}" type="module"></script>'
+        return format_html(template, self.absolute_path(path))
+
 
 class HStoreFormWidget(AdminTextareaWidget):
     def render(self, name, value, attrs=None, renderer=None):
         if attrs is None:
             attrs = {}
-            # it's called "original" because it will be replaced by a copy
 
         # get default HTML from AdminTextareaWidget
         html = super().render(name, value, attrs)
@@ -15,7 +57,7 @@ class HStoreFormWidget(AdminTextareaWidget):
         # prepare template context
         template_context = {
             "field_name": name,
-            "field_data": value,
+            "field_data": value or {},
         }
 
         # get template object
@@ -28,3 +70,5 @@ class HStoreFormWidget(AdminTextareaWidget):
         html = mark_safe(html)
 
         return html
+    class Media:
+        js = [ESM("admin/js/django_hstore_widget/django-hstore-widget.esm.js")]
