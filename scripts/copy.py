7a5304372462ae12530690dc3e6e1c491f7ad46b
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-06-09 10:39:04 +0600

    Add

--- scripts/copy.py
+++ scripts/copy.py
@@ -1,16 +1,10 @@
 import shutil
-import brotli
-import mmap
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-DIST_DIR = Path(
-    BASE_DIR.parent,
-    "dist",
-    "django-hstore-widget",
-)
-
+DIST_DIR = Path(BASE_DIR.parent, "dist", "django-hstore-widget")
 TARGET_DIR = Path(
     BASE_DIR.parent,
     "src",
@@ -20,7 +14,6 @@ TARGET_DIR = Path(
     "js",
     "django_hstore_widget",
 )
-
 if TARGET_DIR.exists() and TARGET_DIR.is_dir():
     shutil.rmtree(TARGET_DIR)
 
@@ -28,18 +21,11 @@ TARGET_DIR.mkdir(parents=True, exist_ok=True)
 
 files = list(DIST_DIR.glob("*.js"))
 
-if not files:
-    raise Exception("There are no js files. Re-run `npm build`")
+if len(files):
+    for js_file in files:
+        if os.path.getsize(js_file) > 0:
+            shutil.copy(js_file, os.path.join(TARGET_DIR, js_file.name))
 
-for js_file in files:
-    if js_file.stat().st_size == 0:
-        continue
-
-    with js_file.open("rb") as f:
-        with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
-            compressed_data = brotli.compress(mm)
-
-    compressed_file = TARGET_DIR / f"{js_file.name}.br"
-    compressed_file.write_bytes(compressed_data)
-
-print("DONE")
+    print("DONE")
+else:
+    raise Exception("There are no js files.Re-Run `npm build`")
--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ from django.utils.encoding import force_str
 @pytest.fixture(scope="session")
 def driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1200")
 
     # Set up console logging preferences
