45407a1443bca580ec82ada7ad0e0fd63a2acf0f
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2025-06-09 09:56:45 +0600

    add

--- scripts/copy.py
+++ scripts/copy.py
@@ -1,10 +1,16 @@
 import shutil
-import os
+import brotli
+import mmap
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-DIST_DIR = Path(BASE_DIR.parent, "dist", "django-hstore-widget")
+DIST_DIR = Path(
+    BASE_DIR.parent,
+    "dist",
+    "django-hstore-widget",
+)
+
 TARGET_DIR = Path(
     BASE_DIR.parent,
     "src",
@@ -14,6 +20,7 @@ TARGET_DIR = Path(
     "js",
     "django_hstore_widget",
 )
+
 if TARGET_DIR.exists() and TARGET_DIR.is_dir():
     shutil.rmtree(TARGET_DIR)
 
@@ -21,11 +28,18 @@ TARGET_DIR.mkdir(parents=True, exist_ok=True)
 
 files = list(DIST_DIR.glob("*.js"))
 
-if len(files):
-    for js_file in files:
-        if os.path.getsize(js_file) > 0:
-            shutil.copy(js_file, os.path.join(TARGET_DIR, js_file.name))
+if not files:
+    raise Exception("There are no js files. Re-run `npm build`")
 
-    print("DONE")
-else:
-    raise Exception("There are no js files.Re-Run `npm build`")
+for js_file in files:
+    if js_file.stat().st_size == 0:
+        continue
+
+    with js_file.open("rb") as f:
+        with mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ) as mm:
+            compressed_data = brotli.compress(mm)
+
+    compressed_file = TARGET_DIR / f"{js_file.name}.br"
+    compressed_file.write_bytes(compressed_data)
+
+print("DONE")
